f27e4673011294695ae04ddb684d494f131872d5
Tommy Craft <me@tommyc.net>
2019-01-09 16:38:34 -0800

    Initial commit

---
+++ app/src/main/java/net/tommyc/android/tourofhonor/captureBonus.java
@@ -0,0 +1,13 @@
+package net.tommyc.android.tourofhonor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class captureBonus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture_bonus);
+    }
+}
