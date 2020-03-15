9372f2cf1561e0f55850f0afc2c9d31fad642c7c
DJFriar <me@tommyc.net>
2020-03-15 16:22:39 -0500

    IDE update and migration to Android X. I also added version.properties and the DB to this commit.

--- app/src/main/java/net/tommyc/android/tourofhonor/bonusDetail.java
+++ app/src/main/java/net/tommyc/android/tourofhonor/bonusDetail.java
@@ -2,8 +2,8 @@ package net.tommyc.android.tourofhonor;
 
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.widget.TextView;
 
